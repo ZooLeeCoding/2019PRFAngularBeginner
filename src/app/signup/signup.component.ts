fd1c4bf3d673af66c6992baa43564585bdfb3db2
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-03-12 18:52:51 +0100

    custom component finished

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service'; 
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  callGreet(): String {
+    return this.userService.greetUser();
   }
 
 }
